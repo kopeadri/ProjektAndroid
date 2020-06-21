8f901bda00ebef6bbc9e503d759d0fec4e709f4d
Klaudia Rychter <rychter.kl@gmail.com>
2020-06-21 18:39:46 +0200

    push notifications

--- FloWatering/app/src/main/java/com/example/flowatering/MainActivity.java
+++ FloWatering/app/src/main/java/com/example/flowatering/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.flowatering;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.example.flowatering.MyApplication.CHANNEL_1_ID;
+import static com.example.flowatering.MyApplication.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity implements PlantsListFragment.PlantsListFragmentActivityListener {
-
+    private NotificationManagerCompat notificationManager;
     MyBroadcastReceiver broadcastReceiver = new MyBroadcastReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        notificationManager = NotificationManagerCompat.from(this);
         IntentFilter filter = new IntentFilter("com.example.MY_ACTION");
         registerReceiver(broadcastReceiver, filter);
+
+        NotifyToCheckAllPlants();
+        plantWarningNotif("Kaktus Krzyś");
     }
 
     @Override
@@ -35,6 +49,42 @@ public class MainActivity extends AppCompatActivity implements PlantsListFragmen
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(broadcastReceiver);
+    }
+
+    public void NotifyToCheckAllPlants() {
+        String title = "Twoje roślinki potrzebują uwagi!";
+        String message = "Sprawdź stan nawodnienia swoich roślin";
+
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setContentIntent(contentIntent)
+                .build();
+        notificationManager.notify(1, notification);
+    }
+
+    public void plantWarningNotif(String plantname) {
+        String title = plantname+" potrzebuje wody!";
+        String message = "Twoja roślinka o imieniu "+plantname+" już dawno nie była podlewana";
+
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
+                .build();
+        notificationManager.notify(2, notification);
     }
 
 }
--- FloWatering/app/src/main/java/com/example/flowatering/MyApplication.java
+++ FloWatering/app/src/main/java/com/example/flowatering/MyApplication.java
@@ -1,18 +1,43 @@
 package com.example.flowatering;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 
 public class MyApplication extends Application {
 
     private static Context context;
-
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
     public void onCreate() {
         super.onCreate();
+        createNotificationChannels();
         MyApplication.context = getApplicationContext();
     }
 
     public static Context getAppContext() {
         return MyApplication.context;
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1 low",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel1.setDescription("Remind to check on current plant states");
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Channel 2 high",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel2.setDescription("Notify important changes in plants' states");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+        }
     }
 }
